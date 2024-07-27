db54571b5e921957ee4348f640111f7cdebca17b
alvescode <videoliva@gmail.com>
2024-07-26 22:17:26 -0300

    add .env file

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,25 @@ import "reflect-metadata";
 import { AppDataSource } from "./database/data-source.js";
 import routers from "./app/routes/routes.js";
 import cors from "cors";
+import dotenv from "dotenv";
 
 const server = express();
 server.use(cors());
 server.use(express.json());
 server.use(routers);
-const PORT = 3001;
+
+const result = dotenv.config();
+if (result.error) {
+  throw result.error;
+}
+
+console.log(result.parsed);
 
 AppDataSource.initialize()
   .then(() => {
     console.log("Data Source inicializado!");
-    server.listen(PORT, () =>
-      console.log(`Servidor rodando na porta ${PORT}.`),
+    server.listen(process.env.PORT, () =>
+      console.log(`Servidor rodando na porta ${process.env.PORT}.`),
     );
   })
   .catch((err) => {
--- src/tests/integração/get.test.ts
+++
@@ -1,4 +0,0 @@
-test("GET para /status deve retornar 200", async () => {
-  const response = await fetch("http://localhost:3001/status");
-  expect(response.status).toBe(200);
-});
